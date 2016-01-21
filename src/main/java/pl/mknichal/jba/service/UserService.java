307eaf57dada126b501fb17eeb692be9d3508c94
Knichu <knichu93@gmail.com>
2016-01-21 22:46:15 +0100

    #11 Added Spring Web MVC with Apache Tiles. Styling table by Bootsrap

---
+++ src/main/java/pl/mknichal/jba/service/UserService.java
@@ -0,0 +1,20 @@
+package pl.mknichal.jba.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.mknichal.jba.entity.User;
+import pl.mknichal.jba.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+}
--- src/main/webapp/WEB-INF/layout/footer.jsp
+++ src/main/webapp/WEB-INF/layout/footer.jsp
@@ -1,4 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 
-&copy; Created by Knichał Maciej powered by Jetty and SpringFramework	
+&copy; Created by Knichał Maciej
+&lt; Powered by SpringFramework, Jetty, Bootsrap &gt;
