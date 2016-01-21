5c28ec24e2f9d6b3190d60b5e7588d523c720cec
Knichu <knichu93@gmail.com>
2016-01-22 00:14:28 +0100

    #17 Spring Data JPA for paging. Added to SQL "Select.. ORDER BY... LIMIT"

--- src/main/java/pl/mknichal/jba/repository/BlogRepository.java
+++ src/main/java/pl/mknichal/jba/repository/BlogRepository.java
@@ -1,9 +1,14 @@
 package pl.mknichal.jba.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pl.mknichal.jba.entity.Blog;
+import pl.mknichal.jba.entity.User;
 
 public interface BlogRepository extends JpaRepository<Blog, Integer>{
 
+	List<Blog> findByUser(User user);
+	
 }
